8f601a9c0a22a59983aa69a955570ca38702306e
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-10-02 08:33:55 -0600

    Notificaciones

--- InterfacesForm/Form1.Designer.cs
+++ InterfacesForm/Form1.Designer.cs
@@ -31,27 +31,29 @@
             label1 = new Label();
             cboTipoVehiculo = new ComboBox();
             groupBox1 = new GroupBox();
-            btnArrancar = new Button();
-            btnDetener = new Button();
             btnCargarDescargar = new Button();
+            btnDetener = new Button();
+            btnArrancar = new Button();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(39, 31);
+            label1.Location = new Point(34, 23);
             label1.Name = "label1";
-            label1.Size = new Size(119, 20);
+            label1.Size = new Size(94, 15);
             label1.TabIndex = 0;
             label1.Text = "Tipo de vehículo";
             // 
             // cboTipoVehiculo
             // 
+            cboTipoVehiculo.DropDownStyle = ComboBoxStyle.DropDownList;
             cboTipoVehiculo.FormattingEnabled = true;
-            cboTipoVehiculo.Location = new Point(204, 28);
+            cboTipoVehiculo.Location = new Point(178, 21);
+            cboTipoVehiculo.Margin = new Padding(3, 2, 3, 2);
             cboTipoVehiculo.Name = "cboTipoVehiculo";
-            cboTipoVehiculo.Size = new Size(151, 28);
+            cboTipoVehiculo.Size = new Size(133, 23);
             cboTipoVehiculo.TabIndex = 1;
             // 
             // groupBox1
@@ -59,48 +61,57 @@
             groupBox1.Controls.Add(btnCargarDescargar);
             groupBox1.Controls.Add(btnDetener);
             groupBox1.Controls.Add(btnArrancar);
-            groupBox1.Location = new Point(25, 83);
+            groupBox1.Location = new Point(22, 62);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(526, 94);
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(460, 87);
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
             groupBox1.Text = "Acciones";
             // 
-            // btnArrancar
-            // 
-            btnArrancar.Location = new Point(20, 41);
-            btnArrancar.Name = "btnArrancar";
-            btnArrancar.Size = new Size(135, 29);
-            btnArrancar.TabIndex = 0;
-            btnArrancar.Text = "Arrancar";
-            btnArrancar.UseVisualStyleBackColor = true;
-            // 
-            // btnDetener
-            // 
-            btnDetener.Location = new Point(195, 41);
-            btnDetener.Name = "btnDetener";
-            btnDetener.Size = new Size(135, 29);
-            btnDetener.TabIndex = 1;
-            btnDetener.Text = "Detener";
-            btnDetener.UseVisualStyleBackColor = true;
-            // 
             // btnCargarDescargar
             // 
-            btnCargarDescargar.Location = new Point(372, 41);
+            btnCargarDescargar.Location = new Point(326, 31);
+            btnCargarDescargar.Margin = new Padding(3, 2, 3, 2);
             btnCargarDescargar.Name = "btnCargarDescargar";
-            btnCargarDescargar.Size = new Size(135, 29);
+            btnCargarDescargar.Size = new Size(118, 35);
             btnCargarDescargar.TabIndex = 2;
             btnCargarDescargar.Text = "Cargar/Descargar";
             btnCargarDescargar.UseVisualStyleBackColor = true;
+            btnCargarDescargar.Click += btnCargarDescargar_Click;
+            // 
+            // btnDetener
+            // 
+            btnDetener.Location = new Point(171, 31);
+            btnDetener.Margin = new Padding(3, 2, 3, 2);
+            btnDetener.Name = "btnDetener";
+            btnDetener.Size = new Size(118, 35);
+            btnDetener.TabIndex = 1;
+            btnDetener.Text = "Detener";
+            btnDetener.UseVisualStyleBackColor = true;
+            btnDetener.Click += btnDetener_Click;
+            // 
+            // btnArrancar
+            // 
+            btnArrancar.Location = new Point(18, 31);
+            btnArrancar.Margin = new Padding(3, 2, 3, 2);
+            btnArrancar.Name = "btnArrancar";
+            btnArrancar.Size = new Size(120, 35);
+            btnArrancar.TabIndex = 0;
+            btnArrancar.Text = "Arrancar";
+            btnArrancar.UseVisualStyleBackColor = true;
+            btnArrancar.Click += btnArrancar_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(580, 214);
+            ClientSize = new Size(508, 160);
             Controls.Add(groupBox1);
             Controls.Add(cboTipoVehiculo);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             groupBox1.ResumeLayout(false);
             ResumeLayout(false);
---
+++ NotificacionesForm/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace NotificacionesForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            btnAgregar = new Button();
+            label3 = new Label();
+            txtMensaje = new TextBox();
+            btnEnviarSMS = new Button();
+            btnEnviarEmail = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(259, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(154, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Notificaciones";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(60, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtUsuario.Location = new Point(175, 80);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(340, 29);
+            txtUsuario.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAgregar.Location = new Point(548, 80);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(95, 29);
+            btnAgregar.TabIndex = 3;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(60, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Mensaje:";
+            // 
+            // txtMensaje
+            // 
+            txtMensaje.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtMensaje.Location = new Point(175, 135);
+            txtMensaje.Multiline = true;
+            txtMensaje.Name = "txtMensaje";
+            txtMensaje.Size = new Size(340, 185);
+            txtMensaje.TabIndex = 5;
+            // 
+            // btnEnviarSMS
+            // 
+            btnEnviarSMS.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEnviarSMS.Location = new Point(175, 354);
+            btnEnviarSMS.Name = "btnEnviarSMS";
+            btnEnviarSMS.Size = new Size(136, 35);
+            btnEnviarSMS.TabIndex = 6;
+            btnEnviarSMS.Text = "Enviar por SMS";
+            btnEnviarSMS.UseVisualStyleBackColor = true;
+            // 
+            // btnEnviarEmail
+            // 
+            btnEnviarEmail.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEnviarEmail.Location = new Point(384, 354);
+            btnEnviarEmail.Name = "btnEnviarEmail";
+            btnEnviarEmail.Size = new Size(131, 35);
+            btnEnviarEmail.TabIndex = 7;
+            btnEnviarEmail.Text = "Enviar por Email";
+            btnEnviarEmail.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(701, 422);
+            Controls.Add(btnEnviarEmail);
+            Controls.Add(btnEnviarSMS);
+            Controls.Add(txtMensaje);
+            Controls.Add(label3);
+            Controls.Add(btnAgregar);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "MainForm";
+            Text = "MainForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private Button btnAgregar;
+        private Label label3;
+        private TextBox txtMensaje;
+        private Button btnEnviarSMS;
+        private Button btnEnviarEmail;
+    }
+}
